412e298bb8824ddb379a6d40a644a12c06a6c4cf
Nicolas Blackburn <nicolas@nblackburn.ca>
2017-07-15 10:05:20 -0400

    Maudit merge...

--- src/trackerml/XMPattern.cpp
+++ src/trackerml/XMPattern.cpp
@@ -1,7 +1,13 @@
-
 #include "XMPattern.h"
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <vector>
 
 namespace trackerml {
 
-    using namespace std;
+    std::string PatternData::toString() { return ""; }
+
+    std::vector<std::vector<boost::shared_ptr<PatternData>>> & XMPattern::getChannels() {
+        return this->channels;
+    }
 }
